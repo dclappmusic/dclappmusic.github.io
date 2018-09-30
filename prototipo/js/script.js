05cce944bf50cbf5fbcbcff8abafb8a981054b54
rober <japimes@gmail.com>
2018-09-30 20:31:53 +0200

    user y band DB
    crear usuario y pasarlo como parámetro, y crear banda y metérselo al user tb

--- prototipo/js/script.js
+++ prototipo/js/script.js
@@ -84,6 +84,23 @@
             }
         };
 
+
+    //Coger parámetro de la URL
+        var getUrlParameter = function getUrlParameter(sParam) {
+			var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+				sURLVariables = sPageURL.split('&'),
+				sParameterName,
+				i;
+
+			for (i = 0; i < sURLVariables.length; i++) {
+				sParameterName = sURLVariables[i].split('=');
+
+				if (sParameterName[0] === sParam) {
+					return sParameterName[1] === undefined ? true : sParameterName[1];
+				}
+			}
+		};
+
 //----------------------------------------------------------
 
     //comparar distancias dadas coordenadas del clapp y del show
