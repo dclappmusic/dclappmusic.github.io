0211c0fce2a572c5e42d709398873fd14b06cb24
rober <japimes@gmail.com>
2019-05-22 00:37:04 +0200

    pa fusionar

--- beta/js/firebase-config.js
+++ beta/js/firebase-config.js
@@ -1,13 +1,21 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyDG0-qVAOkxpGbJaLuctrJE3xTSVR2QDMk",
-  authDomain: "dclapp-49969.firebaseapp.com",
-  databaseURL: "https://dclapp-49969.firebaseio.com",
-  projectId: "dclapp-49969",
-  storageBucket: "dclapp-49969.appspot.com",
-  messagingSenderId: "346881274485"
-};
-firebase.initializeApp(config);
+var app_firebase = {};
+(function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB-XrsVnfHLPLNU5lNXo2-3x_IjV94Iph8",
+    authDomain: "dclapp-49969.firebaseapp.com",
+    databaseURL: "https://dclapp-49969.firebaseio.com",
+    projectId: "dclapp-49969",
+    storageBucket: "dclapp-49969.appspot.com",
+    messagingSenderId: "346881274485"
+  };
+  firebase.initializeApp(config);
+
+  app_firebase = firebase;
+})();
+
+
+
 
 const firestore = firebase.firestore();
 const settings = {/* your settings... */ timestampsInSnapshots: true};
