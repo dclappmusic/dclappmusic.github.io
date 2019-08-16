0bce04befe26ac34056410de3f3febcb3795ca77
Rober <roberto@estudio143.com>
2019-08-16 21:52:31 +0200

    con maps

--- prototipo_vue/src/components/Map.vue
+++ prototipo_vue/src/components/Map.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="page maps" data-page="maps">
        <h1 class="cabecera">MAP</h1>
-       <div id="map"></div>
+       <div class="google-map" id="map"></div>
     </div>
 </template>
 
@@ -20,51 +20,22 @@ export default {
         }
     },
     async mounted() {
-        const locations = [
-            {
-                position: {
-                    lat: 40.432306,
-                    lng: -3.693502
-                },
-            },
-            {
-                position: {
-                    lat: 40.444167,
-                    lng: -3.709342
-                },
-            }
-        ];
         try {
             const google = await gmapsInit();
-            const geocoder = new google.maps.Geocoder();
-            const map = new google.maps.Map(this.$el);
-
-            geocoder.geocode({ address: 'Madrid' }, (results, status) => {
-                if (status !== 'OK' || !results[0]) {
-                    throw new Error(status);
-                }
-                map.setCenter(results[0].geometry.location);
-                map.fitBounds(results[0].geometry.viewport);
-            });
-            const markers = locations
-                .map((location) => {
-                    const marker = new google.maps.Marker({ ...location, map });
-                    marker.addListener('click', () => markerClickHandler(marker));
-                        return marker;
-                });
-            new MarkerClusterer(map, markers, {
-                magePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
-            });
+            var uluru = {lat: -25.344, lng: 131.036};
+            var map = new google.maps.Map(
+                 document.getElementById('map'), {zoom: 4, center: uluru});
+            var marker = new google.maps.Marker({position: uluru, map: map});
         } catch (error) {
             console.error(error);
         }
-    },
+    }
 }
 </script>
 <style lang="scss" >
     .maps {
         .cabecera {color: $primario; margin-bottom: 5%;}
-        .map {
+        #map {
             width: 100vw;
             height: 100vh;
         }
