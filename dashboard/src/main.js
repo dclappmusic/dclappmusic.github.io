298e1d13f312614089a3e135e57bafda9e2aebd1
Rober <roberto@estudio143.com>
2020-05-18 00:50:39 +0200

    dashboard 1.0

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -2,20 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import firebase from 'firebase'
+import router from './router';
+import store from './store';
+import moment from 'moment';
+//General styles
+import "@/css/styles.scss"
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.prototype.$moment = moment;
 
 var firebaseConfig = {
     apiKey: "AIzaSyCcFHmQQSZQMCLlNetLQbW2T4Z7AKH6ztc",
     authDomain: "weclapp-eddbd.firebaseapp.com",
     databaseURL: "https://weclapp-eddbd.firebaseio.com",
     projectId: "weclapp-eddbd",
-    storageBucket: "",
+    storageBucket: "weclapp-eddbd.appspot.com",
     messagingSenderId: "611930943683",
     appId: "1:611930943683:web:c50aff0449c4ad6a"
 };
 firebase.initializeApp(firebaseConfig);
 
 new Vue({
-  render: h => h(App),
+    router,
+    store,
+    render: h => h(App),
 }).$mount('#app')
---
+++ dashboard/src/store.js
@@ -0,0 +1,46 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        shows: [],
+        bands: [],
+        venues: [],
+        user: null,
+        geolocation: {},
+        city: null
+    },
+    getters: {
+    },
+    mutations: {
+        updateUser(state, user) {
+            state.user = user;
+        },
+        updateShows(state, shows) {
+        //before store it, sort them by time
+            if (shows) {
+                shows.sort(function(a,b){
+                    return new Date(a.timestamp) - new Date(b.timestamp);
+                });
+            }
+            state.shows = shows;
+        },
+        updateBands(state, bands) {
+            state.bands = bands;
+        },
+        updateVenues(state, venues) {
+            state.venues = venues;
+        },
+        updateGeolocation(state, geolocation) {
+            state.geolocation = geolocation;
+        },
+        updateCity(state, city) {
+            state.city = city;
+        }
+    },
+    actions: {
+
+    }
+})
