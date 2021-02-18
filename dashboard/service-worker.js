17fcfc1f5e87b88592ba23a6a77f138827b7cada
Rober <roberto@estudio143.com>
2021-02-18 20:52:32 +0100

    deployment a PRO

--- dashboard/service-worker.js
+++ dashboard/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dashboard/precache-manifest.7983681b4fa89d927a073eff2c43b3ec.js"
+  "/dashboard/precache-manifest.bbaa5bbe8dcb3bdbf97fc55b9dba4e19.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "dashboard"});
