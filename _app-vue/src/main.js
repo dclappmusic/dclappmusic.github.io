79da344018c5b26ae6c8b67b3bb9b588bd48eec4
Rober <roberto@estudio143.com>
2021-03-17 11:28:53 +0100

    app > añadido service worker y plugin PWA

--- _app-vue/src/main.js
+++ _app-vue/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-// import './registerServiceWorker';
+import './registerServiceWorker';
 import moment from 'moment';
 import VueI18n from 'vue-i18n'
 import Vue2TouchEvents from 'vue2-touch-events'
@@ -22,17 +22,6 @@ moment.locale('es');
 Vue.prototype.$moment = moment;
 
 Vue.use(Vue2TouchEvents);
-
-// var firebaseConfig = {
-//     apiKey: "AIzaSyCcFHmQQSZQMCLlNetLQbW2T4Z7AKH6ztc",
-//     authDomain: "weclapp-eddbd.firebaseapp.com",
-//     databaseURL: "https://weclapp-eddbd.firebaseio.com",
-//     projectId: "weclapp-eddbd",
-//     storageBucket: "weclapp-eddbd.appspot.com",
-//     messagingSenderId: "611930943683",
-//     appId: "1:611930943683:web:c50aff0449c4ad6a"
-// };
-// firebase.initializeApp(firebaseConfig);
 
 //adds get week number to the date method
 Date.prototype.getWeekNumber = function(){
---
+++ app/precache-manifest.d31670a9ba9cde36120aaad3daa838f9.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa8db76572e431f8549",
+    "url": "/app/css/app.8c34a9c9.css"
+  },
+  {
+    "revision": "fcd1f10e8a413c3d5b03",
+    "url": "/app/css/chunk-03cf1f4a.82d0db3a.css"
+  },
+  {
+    "revision": "b1499061dd8c9886489a",
+    "url": "/app/css/chunk-3e42219a.3da99801.css"
+  },
+  {
+    "revision": "1a22525e96ed3f5377b5",
+    "url": "/app/css/chunk-ad4ae05c.d99fd759.css"
+  },
+  {
+    "revision": "64fe8bb8b861a5d0cfd7",
+    "url": "/app/css/chunk-f8f82884.305338e3.css"
+  },
+  {
+    "revision": "3bd3ac841f52e87d020e",
+    "url": "/app/css/chunk-fd9512e4.0fb85c8b.css"
+  },
+  {
+    "revision": "e059ade26a46c3fb3aff219ae53d709e",
+    "url": "/app/fonts/Changa-Bold.e059ade2.ttf"
+  },
+  {
+    "revision": "c1a282bf2e734550ce86099d96eb0ba1",
+    "url": "/app/fonts/Changa-Light.c1a282bf.ttf"
+  },
+  {
+    "revision": "14f5f301e600a9357882b92b4a189b20",
+    "url": "/app/fonts/Changa-Medium.14f5f301.ttf"
+  },
+  {
+    "revision": "a6aa4fb891b5d078969d1e0c3a7e0135",
+    "url": "/app/fonts/Changa-Regular.a6aa4fb8.ttf"
+  },
+  {
+    "revision": "685eefb3c10874ee3c24c06be82a82bc",
+    "url": "/app/fonts/Changa-SemiBold.685eefb3.ttf"
+  },
+  {
+    "revision": "e22062b3188c8199283ef2aa835d4653",
+    "url": "/app/fonts/Roboto-Light.e22062b3.ttf"
+  },
+  {
+    "revision": "99fc0816a09395454061301fefa42bf1",
+    "url": "/app/fonts/Roboto-Medium.99fc0816.ttf"
+  },
+  {
+    "revision": "54a91b0619ccf9373d525109268219dc",
+    "url": "/app/fonts/Roboto-Regular.54a91b06.ttf"
+  },
+  {
+    "revision": "02bd531a07a5a374651ab76ef033601b",
+    "url": "/app/images/avatar_base.png"
+  },
+  {
+    "revision": "36f1d9976bb250bd72a11cfa30230199",
+    "url": "/app/images/avatar_placeholder.jpg"
+  },
+  {
+    "revision": "369254abd9dba2886a7874e242113111",
+    "url": "/app/images/clapp-rojo.png"
+  },
+  {
+    "revision": "fcec6f5152dba4d6075d7d34689ea5d5",
+    "url": "/app/images/facebook.svg"
+  },
+  {
+    "revision": "2d7ec526e2845fc4df963a91b85d8227",
+    "url": "/app/images/favicon.png"
+  },
+  {
+    "revision": "36389f5cb6c569ce9ee9d465b5607c68",
+    "url": "/app/images/google.svg"
+  },
+  {
+    "revision": "2b58be4fd9d1744adf33a10f5e855964",
+    "url": "/app/images/historial_preview.png"
+  },
+  {
+    "revision": "b7e9f8c16789d3297dcf2e3f927ab0d0",
+    "url": "/app/images/icon-edit.svg"
+  },
+  {
+    "revision": "eaa786835fdf3df7e8e3665fa11a956e",
+    "url": "/app/images/icon-options.svg"
+  },
+  {
+    "revision": "df9cb61ab8e18b32cff85c2f006b6459",
+    "url": "/app/images/icon-save.svg"
+  },
+  {
+    "revision": "332c2ab57d9ecfdc3b4f177c254af20e",
+    "url": "/app/images/icon_clapp.png"
+  },
+  {
+    "revision": "e10eecd1e7e3b92702bf3246f4b57e14",
+    "url": "/app/images/icon_completo.png"
+  },
+  {
+    "revision": "53dee859715f0520e4214c63a73700e6",
+    "url": "/app/images/icon_rrss_facebook.svg"
+  },
+  {
+    "revision": "e7894605485353c30d00dab2090823e0",
+    "url": "/app/images/icon_rrss_instagram.svg"
+  },
+  {
+    "revision": "8fa7791615266bcf9874fc20626c4e64",
+    "url": "/app/images/icon_rrss_link.svg"
+  },
+  {
+    "revision": "01a08b8d7685b6051101fbecb575d21e",
+    "url": "/app/images/icon_rrss_soundcloud.svg"
+  },
+  {
+    "revision": "6a083ad3c422c48de4fd5bdcbb35ce0f",
+    "url": "/app/images/icon_rrss_spotify.svg"
+  },
+  {
+    "revision": "c85c7e1e8b88f81e4abd080531e246ee",
+    "url": "/app/images/icon_rrss_youtube.svg"
+  },
+  {
+    "revision": "f9d1cecd69c397f71a1c7b3d6c920d10",
+    "url": "/app/images/perfil_kurt.png"
+  },
+  {
+    "revision": "9054a35980dadf7e787c2c086bb404d1",
+    "url": "/app/images/pin_liveshow.png"
+  },
+  {
+    "revision": "b0aa12455dfa0278c26024eebddf10fb",
+    "url": "/app/images/pin_liveshow_white.png"
+  },
+  {
+    "revision": "287ef0b1eaac02da98a2866ffcb930fa",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "2aa8db76572e431f8549",
+    "url": "/app/js/app.e54eb022.js"
+  },
+  {
+    "revision": "fcd1f10e8a413c3d5b03",
+    "url": "/app/js/chunk-03cf1f4a.8eb48bef.js"
+  },
+  {
+    "revision": "2d007590ee3ba57c2c13",
+    "url": "/app/js/chunk-2d0e5772.316ff7df.js"
+  },
+  {
+    "revision": "1d472ed374e4a89f1ea5",
+    "url": "/app/js/chunk-3607be1e.c77038bc.js"
+  },
+  {
+    "revision": "b1499061dd8c9886489a",
+    "url": "/app/js/chunk-3e42219a.c0d6f324.js"
+  },
+  {
+    "revision": "1a22525e96ed3f5377b5",
+    "url": "/app/js/chunk-ad4ae05c.57e089f9.js"
+  },
+  {
+    "revision": "64fe8bb8b861a5d0cfd7",
+    "url": "/app/js/chunk-f8f82884.fdc89fba.js"
+  },
+  {
+    "revision": "3bd3ac841f52e87d020e",
+    "url": "/app/js/chunk-fd9512e4.3be5d3e2.js"
+  },
+  {
+    "revision": "30181f31b8ace08f433c",
+    "url": "/app/js/chunk-vendors.8fc63082.js"
+  },
+  {
+    "revision": "8addaa12ea2985e2e21a75edb2912916",
+    "url": "/app/js/clapp.js"
+  },
+  {
+    "revision": "24ebe893754fd9b88f14f99442d78b0b",
+    "url": "/app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/app/robots.txt"
+  }
+]);
