4d2bf0695c1aacd6170159e092533a0d7e402b57
Rober <roberto@estudio143.com>
2021-04-13 20:00:13 +0200

    app > fix del la subruta al cambiar lo de los services workers

--- _app-vue/src/views/Manifest.vue
+++ _app-vue/src/views/Manifest.vue
@@ -5,16 +5,18 @@
       </div>
       <div class="view">
         <p class="parr">No somos ni pertenecemos a ninguna corporación, ni tenemos ningún plan secreto para utilizar vuestros datos para venderlos a EvilCorp.</p>
-        <p class="parr">Somos un equipo pequeño muy frikis de la música en directo y de las bandas genuinas, y queremos poner nuestro granito (o granazo si puede ser) de arena para mejorar una escena en riesgo</p>
-        <p class="parr">Creamos weclapp en 2018 tras saturarnos de escucharnos decir estas frases:</p>
+        <p class="parr">Somos un equipo pequeño muy frikis de la música en directo y de las bandas genuinas que queremos poner nuestro granito (o granazo si puede ser) de arena para mejorar una escena en riesgo.</p>
+        <p class="parr">Creamos weclapp en 2018 tras la saturación de escucharnos decir estas frases:</p>
         <ul>
           <li class="parr">Este concierto ha sido la leche, le daría todo mi escaso dinero a la banda para que siguiese tocando.</li>
           <li class="parr">Y oootro festival indie en verano con los mismos 5 grupos que entre todos suman 2 discos.</li>
-          <li class="parr">¿Cómo es que ha este grupazo sólo han ido a verles 25 personas?</li>
+          <li class="parr">¿Cómo es que a este grupazo sólo han ido a verles 25 personas?</li>
           <li class="parr">Otro grupo que se disuelve porque no les da ni para púas.</li>
           <li class="parr">“Si es que si no te promocionas en Instagram con videos de postureo no llegas a nada”</li>
         </ul>
-        <p class="parr">Así que decidimos juntarnos y montar esto, pero no de cualquier manera</p>
+        <p class="parr">
+          Así que decidimos juntarnos y montar esto, pero no de cualquier manera. Decidimos hacerlo por nuestra cuenta, desde abajo, y con vuestra ayuda...
+        </p>
       </div>
     </div>
 </template>
--- app/service-worker.js
+++ app/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.cc2b0e3d0e88569f99705a68fd0b9121.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/app/precache-manifest.8e3957e4da7b166b238968cfd2164519.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.setConfig({
   debug: false
