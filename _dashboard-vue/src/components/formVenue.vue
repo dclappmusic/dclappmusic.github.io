8a9ee84e05e2c8cb2a0564f6c5c86eef2ecbec63
Rober <roberto@estudio143.com>
2021-07-02 13:57:27 +0200

    dash > crear venue > input de lat lon con geocoding

--- _dashboard-vue/src/components/formVenue.vue
+++ _dashboard-vue/src/components/formVenue.vue
@@ -5,8 +5,16 @@
     </div>
     <div class="fila band">
       <input class="parr" :disabled="!edited_venue && new_venue.id" placeholder="city" v-model="new_venue.city" />
-      <input class="parr" :disabled="!edited_venue && new_venue.id" placeholder="latitud" v-model="new_venue.lat" />
-      <input class="parr" :disabled="!edited_venue && new_venue.id" placeholder="longitud" v-model="new_venue.lon" />
+      <mapbox-geocoder 
+        class="geocoder"
+        :access-token="access_token"
+        placeholder="dirección"
+        countries="es"
+        language="es"
+        @mb-created="instance => control = instance" 
+        @mb-result="resultado"
+      />
+      <p class="parr coords" v-if="new_venue.lat">{{new_venue.lat + ', ' + new_venue.lon}}</p>
     </div>
     <div class="fila band">
       <input class="parr" :disabled="!edited_venue && new_venue.id" placeholder="logo" v-model="new_venue.logo" />
@@ -18,10 +26,12 @@
 
 <script>
 import { mapState } from 'vuex';
+import MapboxGeocoder from '@studiometa/vue-mapbox-gl/dist/components/MapboxGeocoder';
+import '@mapbox/mapbox-gl-geocoder/lib/mapbox-gl-geocoder.css';
 
 export default {
 	name: 'formVenue',
-	components: {},
+	components: {MapboxGeocoder},
   props: {
     edited_venue: {},
     new_venue: {},
@@ -31,13 +41,41 @@ export default {
 	},
 	data() {
 		return {
-      disable: false
+      disable: false,
+      location: null,
+      reverse_geocode: false,
+      access_token: "pk.eyJ1IjoiamFwaW1lcyIsImEiOiJjazF3cWdma2QwNDZwM2VxdGpldDQxZWlwIn0.NXdh9SyvQKYtfDyIKGy-ZQ",
 		}
 	},
 	created: function() {},
-  mounted: function(){},
-	methods: {}
+  mounted: function() {},
+	methods: {
+    resultado(result) {
+      debugger;
+      this.new_venue.lat = result.result.center[0];
+      this.new_venue.lon = result.result.center[1];
+    },
+  }
 }
 </script>
 <style scoped lang="scss">
+.coords {
+  display: inline-block;
+  margin-left: 1em;
+}
+.geocoder {
+  display: inline-block;
+  /deep/ .mapboxgl-ctrl-geocoder {
+    background-color: transparent;
+    .mapboxgl-ctrl-geocoder--icon-search {
+      height: 2em;
+      left: auto;
+      right: 7px;
+    }
+    input {
+      color: white;
+      font-size: 1em;
+    }
+  }
+}
 </style>
